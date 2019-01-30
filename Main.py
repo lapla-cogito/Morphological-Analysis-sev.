650117c7b5ca4c56c31c0de00491c7c3c8519938
severrabaen <39644970+severrabaen@users.noreply.github.com>
2019-01-30 20:29:06 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,9 @@
 #import
 import tensorflow as tf
 import numpy as np
-import numpy.random as nr
 import matplotlib.pyplot as plt
 import csv
 import pandas as pd
-import random
 import sys
 import h5py
 import math
@@ -21,26 +19,51 @@ from keras.initializers import uniform
 from keras.optimizers import RMSprop
 from keras.utils import np_utils
 
-#input data
+
+#CSVファイルを読み込む
 inp = csv.reader(open('hogehoge.csv','r'))
 
+#宣言
 data = [ i for i in inp]
 gabword=[]
 mon = np.array(data)
 words = sorted(list(set(mon)))
 cou = np.zeros(len(words))
+ma=10
 windex = dict((i, j) for j, i in enumerate(words))
 indexw = dict((i, j) for i, j in enumerate(words))
 
-#count
+#語数を数える
 for i in range(0, len(words)):
     cou[windex[mon[j]]]+=1
-    
+
+#文章中にあまり出てこない単語はゴミなので弾く(NGワードに書き換える)
 for i in range(0, len(words)):
   if cnt[i] <= 3 :
     gabword.append(words[i])
-    words[i] = 'gabgabeidjdkdjekejdhogehogelatte'
+    words[i] = 'gabgabeidjdkdjekejdhogehogelatteukkuku'
 
 words = sorted(list(set(words)))
 windex = dict((i, j) for j, i in enumerate(words))
 indexw = dict((i, j) for i, j in enumerate(words))
+
+#訓練データの作成
+monn=np.zeros((len(mon),1),dtype=int)
+for i in range(0,len(mon)):
+  if mon[i]in windex:
+    monn[i,0]=windex[mon[i]]
+  else:
+    monn[i,0]=windex['gabgabeidjdkdjekejdhogehogelatteukkuku']
+    
+monlen=len(monn)-ma
+target=[]
+
+
+for i in range(ma,monlen):
+  
+
+#ニューラルネットワークの構築
+class neural:
+  def init(self,inpu,outp):
+    self.inpu=inpu
+    self.outp=outp
